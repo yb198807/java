b9ba82d0aeaf3a32f4c8dad9f112574cc26f770b
yebin <yebin4@sina.com>
2023-10-17 17:54:09 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         // 当文本光标位于高亮显示的文本处时按 Opt+Enter，
         // 可查看 IntelliJ IDEA 对于如何修正该问题的建议。
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
 
         // 按 Ctrl+R 或点击装订区域中的绿色箭头按钮以运行脚本。
         for (int i = 1; i <= 5; i++) {
