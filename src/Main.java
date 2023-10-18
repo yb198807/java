c3fbb0224d832eaf2445edfea7ffad1e24afb688
yebin <yebin4@sina.com>
2023-10-18 17:30:36 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 当文本光标位于高亮显示的文本处时按 Opt+Enter，
-        // 可查看 IntelliJ IDEA 对于如何修正该问题的建议。
-        System.out.println("Hello and welcome!");
-
-        // 按 Ctrl+R 或点击装订区域中的绿色箭头按钮以运行脚本。
+        int sum1 = 1, sum2 = 0;
         for (int i = 1; i <= 5; i++) {
-
-            // 按 Ctrl+D 开始调试代码。我们已为您设置了一个断点，
-            // 但您始终可以通过按 Cmd+F8 添加更多断点。
-            System.out.println("i = " + i);
+            sum1=1;
+            for (int j = 1; j <= i; j++) {
+                sum1*=j;
+            }
+            sum2+=sum1;
         }
+        System.out.println(sum2);
+//        Scanner input=new Scanner(System.in);
+//        int a=input.nextInt();
+//        System.out.println(a);
     }
 }
